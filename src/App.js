3113f30dbad84eb2a68a4a260c43d88885695050
praveen <praveenyadav16178@gmail.com>
2024-08-18 20:01:39 +0530

    first commit for github upload file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar title="icoder" abouttext="text" />  */}
+      <Navbar/>
+      <h1>i am navbar</h1>
+    </>
+  );
+}
+
+export default App;
