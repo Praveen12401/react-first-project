0b5cfc382544b031482108f4ddb986fafd0efc01
Praveen <praveenyadav16178@gmail.com>
2025-05-25 14:43:36 +0530

    state working

--- src/component/Textform.js
+++ src/component/Textform.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function Textform() {
+  const [text,setText]=useState('')
+  const handleUpclick=()=>{
+    let newtext=text.toUpperCase()
+    setText(newtext)
+
+  }
+
+   const handleOnchange=(event)=>{
+    setText(event.target.value);
+
+  }
   return (
     <>
     
     <div className='Textform'>
     <h1> Enter text here:  </h1>
  
-  <textarea className="Textform" ></textarea>
+  <textarea className="Textform"  value={text} onChange={handleOnchange}></textarea>
 
-<button type="button" class="btn btn-primary btn-lg">Uppercase</button>
+<button type="button" className="btn btn-primary btn-lg" onClick={handleUpclick}>Uppercase</button>
 </div>
 </>
   )
