b3c73d6b6301baf4c111138a6e7de82855e82e52
Praveen <praveenyadav16178@gmail.com>
2025-05-25 14:15:34 +0530

    not use state of form

---
+++ src/component/Textform.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Textform() {
+  return (
+    <>
+    
+    <div className='Textform'>
+    <h1> Enter text here:  </h1>
+ 
+  <textarea className="Textform" ></textarea>
+
+<button type="button" class="btn btn-primary btn-lg">Uppercase</button>
+</div>
+</>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import  './component/Textform.css';
 import App from './App';
+import {Person,Teacher} from './App';
+
 import reportWebVitals from './reportWebVitals';
+import Navbar from './component/Navbar';
+import Textform from './component/Textform';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     
+    <Navbar title='Navbar'/>
+    <Textform />
+      
   </React.StrictMode>
 );
 
